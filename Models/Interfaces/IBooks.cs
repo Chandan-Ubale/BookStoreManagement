d7b12743757fbb56d27685ffd48ef88735d23752
Chandan G <Chandan.G@cmartsolutions.com>
2025-09-21 15:17:38 +0530

    Restructured solution: added Infra layer, reorganized Core/Services, updated API project

---
+++ Models/Interfaces/IBooks.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStoreManagement.Core.Interfaces
+{
+    public interface IBook
+    {
+        string? Id { get; set; }
+        string? Title { get; set; }
+        string? Author { get; set; }
+        decimal Price { get; set; }
+    }
+}
---
+++ Models/Models/Books.cs
@@ -0,0 +1,22 @@
+﻿using BookStoreManagement.Core.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStoreManagement.Core.Models
+{
+    public class Books : IBook
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Title")]
+        public string? Title { get; set; }
+
+        [BsonElement("Author")]
+        public string? Author { get; set; }
+
+        [BsonElement("Price")]
+        public decimal Price { get; set; }
+    }
+}
